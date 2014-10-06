ec365324b5674ce7032eba7788d43d8af250cf2d
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2014-10-06 22:11:25 +0300

    Fix dependencies function declarations
    closes #3

--- woocommerce-qtml.php
+++ woocommerce-qtml.php
@@ -5,7 +5,7 @@
   Description: Add (m)qTranslate support to WooCommerce.
   Author: SomewhereWarm
   Author URI: http://www.somewherewarm.net
-  Version: 2.0.10
+  Version: 2.0.11
  */
 
 /**
@@ -27,7 +27,7 @@ if ( is_woocommerce_active() ) {
 
 	class WC_QTML {
 
-		var $version = '2.0.10';
+		var $version = '2.0.11';
 
 		var $enabled_languages;
 		var $enabled_locales;
